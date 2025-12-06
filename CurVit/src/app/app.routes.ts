daed6c1fe9ff84f180e76d5bd118b5639521b10b
Paulo Estefano Quenta Ahumada <pquentaa@unsa.edu.pe>
2025-12-05 20:21:27 -0500

    Agregando modificaciones y archivos

--- CurVit/src/app/app.routes.ts
+++ CurVit/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { About } from './features/about/about';
 
 export const routes: Routes = [
     {path: 'home', component: Home},
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'about', component: About},
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
